e7c45ea5cf67ca64df8b4eb01caadc34831a7787
ininahazwe <yves.cri@gmail.com>
2026-02-23 12:26:23 +0000

    tailwinf fix

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -7,6 +7,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+console.log("🚀 Application en cours de chargement...");
+
 // Rendu de l'application
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
